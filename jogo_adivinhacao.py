e75b83cc3c1e4bd3b83557490ba4228a97fb85b2
Marina Thompson <mlbtf@discente.ifpe.edu.br>
2021-09-10 21:12:05 -0300

    finalizando

--- jogo_adivinhacao.py
+++ jogo_adivinhacao.py
@@ -1,13 +1,29 @@
 import random
+from typing import NewType 
 
 print('***********************************')
 print('Bem vindo ao jogo de adivinhação!')
 print('***********************************')
 
-numero_secreto = 12
-total_tentativas = 3
-tentativa_atual = 1
+numero_secreto = random.randrange(1, 101)
+total_tentativas = 0
+pontuacao = 1000
 
+print('Qual o nível de dificuldade você deseja? ')
+print('(1) - Fácil (2) - Médio (3) - Difícil')
+
+nivel = int(input('Digite o nível: '))
+
+if (nivel == 1):
+    total_tentativas = 20
+elif (nivel == 2):
+    total_tentativas = 10
+elif (nivel == 3): 
+    total_tentativas = 5
+else:
+    print('Número inválido, iniciaremos com o nível fácil')
+    total_tentativas = 20
+    
 for tentativa_atual in range(1, total_tentativas + 1):
     print(f'Tentativa {tentativa_atual} de {total_tentativas}')
     numero_usuario = int(input('Digite um número qualquer entre 1 e 100: '))
@@ -21,12 +37,15 @@ for tentativa_atual in range(1, total_tentativas + 1):
     menor = numero_usuario < numero_secreto
 
     if(acertou):
-        print('Parabéns! Você acertou')
+        print(f'Parabéns! Você acertou e fez {pontuacao} pontos.')
         break
     else:
         if(maior):
            print('Número digitado maior que o número secreto.')
+           pontuacao -= numero_usuario - numero_secreto
+           print(pontuacao)
         elif(menor):
           print('Número digitado menor que o número secreto.')
-
+          pontuacao -= numero_secreto - numero_usuario
+          print(pontuacao)
 print('Fim de jogo!')
